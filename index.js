eba7be4cbeef879344de976645bbcf57a9532afe
Che Mok <chemok78@gmail.com>
2017-04-04 04:03:28 +0000

    implement search, random and trending commands

--- index.js
+++ index.js
@@ -5,22 +5,81 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
-const PUBLIC_BETA_KEY = "dc6zaTOxFJmzC";
-const URL = "http://api.giphy.com/v1/gifs/random?api_key=" + PUBLIC_BETA_KEY;
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const server = app.listen(process.env.PORT || 8080, () => {
     console.log('Express server listening on port %d in $s mode', server.address().port, app.settings.env);
 });
 
-app.post('/', (req, res) => {
+/*
+/gif (completely random gif)
+/gif [category] (random gif but related to category)
+/gif -s [search] (specific gif that matches the search)
+/gif -t [1-25] (trending gif, optional specify nr)
+*/
 
-  const searchString = req.body.text.replace(/\s/g, '+');
-  let finalUrl = URL + "&tag=" + searchString;
+const cleanSearch = (text,match) => {
+
+    return text.replace(match, '').replace(/\s/g, '+');
+  
+};
+
+const search = function(text, type, match){
+    
+    //using GIPHY public api key, change when in production
+    const URL = "http://api.giphy.com/v1/gifs/" + type + "?api_key=dc6zaTOxFJmzC";
+    const searchString = cleanSearch(text,match);
+    return URL + "&tag=" + searchString;
+  
+};
+
+const getType = (text, match)  => {
+  
+  if(match === "-t "){ // include a space after to ensure it was a command
+      
+      return "trending";
+    
+  } else if(match === "-s ") { // include a space after to ensure it was a command
+    
+      return "search";
+    
+  }else {
+    
+      return "random";
+    
+  }
+  
+};
+
+const getImageUrl = (body, type) => {
+  
+    if(type === "search"){
+      
+        return body.data[0].fixed_height_downsampled_url;
+        
+    } else if (type === "trending"){
+    //make an array of top 10 images  
+        
+        
+      
+    } else {
+      
+      return body.data.fixed_height_downsampled_url;
+      
+    }
+  
+}
+
+app.post('/', (req, res) => {
+  
+  const searchString = req.body.text.toLowerCase();
+  
+  const match = searchString.match(/^-[ts]\s/);
+  
+  const type = getType(searchString,match);
+  
+  const finalUrl = search(searchString, type, match);
 
   request({
     url: finalUrl,
@@ -28,16 +87,19 @@ app.post('/', (req, res) => {
   }, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
-
-
+    //response ok
+    
       const data = {
         response_type: 'in_channel',
         attachments: [
           {image_url: body.data.fixed_height_downsampled_url}
         ],
       };
+      
       res.json(data); // Print the json response
+      
     }
+    
   });
 
 });
